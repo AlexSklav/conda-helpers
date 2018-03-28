151c46de2d9f67e36a1f12ca5395cf7a2477df5a
Christian Fobel <christian@fobel.net>
2018-03-28 09:56:22 -0400

    feat(run-command): display wait indicator
    Display wait indicator in calls to `run_command(..., verbose=None)`.

--- conda_helpers/_async_py27.py
+++ conda_helpers/_async_py27.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import, print_function, unicode_literals
 from functools import partial
 import io
+import itertools as it
 import subprocess as sp
+import sys
 
+import colorama as _C
 import trollius as asyncio
 
 
@@ -19,6 +22,10 @@ def _read_stream(stream, callback=None):
 
 @asyncio.coroutine
 def run_command(cmd, *args, **kwargs):
+    '''
+    .. versionchanged:: X.X.X
+        Display wait indicator if ``verbose`` is set to ``None`` (default).
+    '''
     shell = kwargs.pop('shell', True)
     verbose = kwargs.pop('verbose', True)
     if isinstance(cmd, list):
@@ -31,18 +38,25 @@ def run_command(cmd, *args, **kwargs):
     stdout_ = io.StringIO()
     stderr_ = io.StringIO()
 
+    message = (_C.Fore.MAGENTA + 'Executing:', _C.Fore.WHITE + cmd)
+    waiting_indicator = it.cycle(r'\|/-')
+
     def dump(output, data):
         text = data.decode('utf8')
         if verbose:
             print(text, end='')
         elif verbose is None:
-            print('.', end='')
+            print('\r' + next(waiting_indicator), *message, end='',
+                  file=sys.stderr)
         output.write(text)
 
-    yield asyncio.From(asyncio.wait([_read_stream(process.stdout, partial(dump,
-                                                                          stdout_)),
-                                     _read_stream(process.stderr, partial(dump,
-                                                                          stderr_))]))
+    yield asyncio.From(asyncio.wait([_read_stream(process.stdout,
+                                                  partial(dump, stdout_)),
+                                     _read_stream(process.stderr,
+                                                  partial(dump, stderr_))]))
+
+    if verbose is None:
+        print('\r' + _C.Fore.GREEN + 'Finished:', message[1], file=sys.stderr)
 
     return_code = yield asyncio.From(process.wait())
     raise asyncio.Return(return_code, stdout_.getvalue(), stderr_.getvalue())
